787bb8c09c484a14261acb23aac6f0273263e8f4
Samir Bagamaev <05ru4315@gmail.com>
2023-08-30 16:31:25 +0300

    EXCEPTIONS SEMINAR 2 HOMEWORK

---
+++ WORK/work1.java
@@ -0,0 +1,24 @@
+/*
+Реализуйте метод, который запрашивает у пользователя ввод дробного числа
+(типа float), и возвращает введенное значение. Ввод текста вместо числа 
+не должно приводить к падению приложения, вместо этого, необходимо повторно 
+запросить у пользователя ввод данных.
+*/
+
+package WORK;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class work1 {
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader bufferedReader = 
+            System.out.println(result);
+        }catch (IOException ex){
+            System.out.println("ОШИБКА!!!");
+            return;
+        }
+    }
+}
---
+++ WORK/work2.java
@@ -0,0 +1,28 @@
+/*
+Если необходимо, исправьте данный код (задание 2
+
+try {
+   int d = 0;
+   double catchedRes1 = intArray[8] / d;
+   System.out.println("catchedRes1 = " + catchedRes1);
+} catch (ArithmeticException e) {
+   System.out.println("Catching exception: " + e);
+}
+*/
+package WORK;
+public class work2 {
+
+    public static void main(String[] args) {
+        try {
+            int d = 0;
+            int[] intArray = new int[10];
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Catching exception: " + e);
+        }
+
+    }
+}
